1c6377e2a99da2ea027e7c0d204ea77a6472aad7
Potatoyz908 <euller2005@gmail.com>
2026-06-12 19:58:04 -0300

    refactor: update social media links and improve footer code formatting

--- components/main/footer.tsx
+++ components/main/footer.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import {
-    RxDiscordLogo,
-    RxGithubLogo,
-    RxInstagramLogo,
-    RxLinkedinLogo,
+  RxDiscordLogo,
+  RxGithubLogo,
+  RxInstagramLogo,
+  RxLinkedinLogo,
 } from "react-icons/rx";
-import { FaWhatsapp} from "react-icons/fa";
+import { FaWhatsapp } from "react-icons/fa";
 import { AiOutlineMail } from "react-icons/ai";
 
 const Footer = () => {
@@ -15,17 +15,17 @@ const Footer = () => {
         <div className="w-full h-full flex flex-row items-center justify-around flex-wrap">
           <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
             <div className="font-bold text-[16px]">Contato</div>
-<div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
-  <p className="flex flex-row items-center my-[15px] cursor-pointer">
-    <a href="https://wa.me/5561991131654" target="_blank" rel="noopener noreferrer" className="flex items-center">
-      <FaWhatsapp />
-      <span className="text-[15px] ml-[6px]">WhatsApp</span>
-    </a>
-  </p>
-</div>
+            <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
+              <p className="flex flex-row items-center my-[15px] cursor-pointer">
+                <a href="https://wa.me/5561991131654" target="_blank" rel="noopener noreferrer" className="flex items-center">
+                  <FaWhatsapp />
+                  <span className="text-[15px] ml-[6px]">WhatsApp</span>
+                </a>
+              </p>
+            </div>
 
             <p className="flex flex-row items-center my-[15px] cursor-pointer">
-              <a href="https://instagram.com/potatoyz1" target="_blank" rel="noopener noreferrer" className="flex items-center">
+              <a href="https://instagram.com/euller.j2" target="_blank" rel="noopener noreferrer" className="flex items-center">
                 <RxInstagramLogo />
                 <span className="text-[15px] ml-[6px]">Instagram</span>
               </a>
@@ -36,38 +36,38 @@ const Footer = () => {
                 <span className="text-[15px] ml-[6px]">Discord</span>
               </a>
             </p>
-<p className="flex flex-row items-center my-[15px] cursor-pointer">
+            <p className="flex flex-row items-center my-[15px] cursor-pointer">
               <a href="mailto:euller2005@gmail.com" className="flex items-center">
                 <AiOutlineMail /> {/* Ícone de email */}
                 <span className="text-[15px] ml-[6px]">euller2005@gmail.com</span>
               </a>
             </p>
           </div>
-          
+
           <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
             <div className="font-bold text-[16px]">Rede Social</div>
             <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
+              <p className="flex flex-row items-center my-[15px] cursor-pointer">
+                <a href="https://www.linkedin.com/in/euller-j%C3%BAlio-002572287/" target="_blank" rel="noopener noreferrer" className="flex items-center">
+                  <RxLinkedinLogo />
+                  <span className="text-[15px] ml-[6px]">LinkedIn</span>
+                </a>
+              </p>
+              <p className="flex flex-row items-center my-[15px] cursor-pointer">
+                <a href="https://wa.me/5561991131654" target="_blank" rel="noopener noreferrer" className="flex items-center">
+                  <FaWhatsapp />
+                  <span className="text-[15px] ml-[6px]">WhatsApp</span>
+                </a>
+              </p>
+            </div>
             <p className="flex flex-row items-center my-[15px] cursor-pointer">
-              <a href="https://www.linkedin.com/in/euller-j%C3%BAlio-002572287/" target="_blank" rel="noopener noreferrer" className="flex items-center">
-                <RxLinkedinLogo />
-                <span className="text-[15px] ml-[6px]">LinkedIn</span>
-              </a>
-            </p>
-  <p className="flex flex-row items-center my-[15px] cursor-pointer">
-    <a href="https://wa.me/5561991131654" target="_blank" rel="noopener noreferrer" className="flex items-center">
-      <FaWhatsapp />
-      <span className="text-[15px] ml-[6px]">WhatsApp</span>
-    </a>
-  </p>
-</div>
-<p className="flex flex-row items-center my-[15px] cursor-pointer">
               <a href="https://github.com/potatoyz908" target="_blank" rel="noopener noreferrer" className="flex items-center">
                 <RxGithubLogo />
                 <span className="text-[15px] ml-[6px]">GitHub</span>
               </a>
             </p>
             <p className="flex flex-row items-center my-[15px] cursor-pointer">
-              <a href="https://instagram.com/potatoyz1" target="_blank" rel="noopener noreferrer" className="flex items-center">
+              <a href="https://instagram.com/euller.j2" target="_blank" rel="noopener noreferrer" className="flex items-center">
                 <RxInstagramLogo />
                 <span className="text-[15px] ml-[6px]">Instagram</span>
               </a>
